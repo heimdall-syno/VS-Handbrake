f5c03ccd332749c1b96cbe6c7865d1abf82e9549
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-01 23:30:47 +0100

    use mediainfo instead of cv2

--- naming_series.py
+++ naming_series.py
@@ -1,20 +1,38 @@
-import sys, glob, os, cv2, re, warnings, csv, argparse, json
-from distutils.util import strtobool
+import sys, os, re, argparse, subprocess
 from shutil import move
 from collections import Counter
 from argparse import Namespace
 
+## Add modules from the submodule (vs-utils)
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(cur_dir, "VS-Utils"))
+sys.path.append(os.path.join(cur_dir, "VS-SynoIndex"))
 from parse import parse_cfg
+from prints import errmsg, debugmsg
 
 season_desc = "Staffel"
 
 config_file = os.path.dirname(os.path.abspath(__file__)) + '/config.txt'
-cfg = parse_cfg(config_file, "server")
+cfg = parse_cfg(config_file, "vs-handbrake", "host")
 
-def get_resolution(res):
+def get_resolution(file_name):
 
-    ## Round the real resolution of the video file
-    ## to the common resolution
+    ## Call mediainfo via command line
+    cmds = ['mediainfo', file_name]
+    p = subprocess.Popen(cmds, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    if (len(stderr) > 1): print(stderr)
+
+    ## Parse the mediainfo output as dictionary
+    mediainfo_output = stdout.split("\n")
+    mediainfo_output = [' '.join(m.split()).split(" : ") for m in mediainfo_output]
+    mediainfo_output = dict([tuple(m) for m in mediainfo_output if len(m) > 1])
+
+    ## Parse the height of the video resolution
+    res = mediainfo_output["Height"]
+    res = int(res.replace(" ", "").replace("pixels", ""))
+
+    ## Round the real resolution of the video file to the common resolution
     if (res == 0): return -1
     if (0 < res and res <= 360): return "360p"
     elif (360 < res and res <= 480): return "480p"
@@ -32,9 +50,7 @@ def analyze_series(series):
     series.extension = os.path.splitext(series.file)[1]
 
     ## Analyze resolution of the season
-    video = cv2.VideoCapture(series.file)
-    height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    series.resolution = get_resolution(height)
+    series.resolution = get_resolution(series.file)
     if(series.resolution == -1):
         print("The resolution of the series episode was invalid")
         exit()
@@ -47,20 +63,13 @@ def analyze_series(series):
     series.season = "%s %s" % (season_desc, series.season)
 
     ## Get the series name of the file
-    directories = series.original.split(os.sep)
-    series.name = directories[directories.index(series.series_path) + 1].replace(" ", "-")
+    series.name = series.original.replace(series.series_path, "").split(os.sep)[1].replace(" ", "-")
 
     ## Get the destination directory
-    series.dst = "%s/" % os.sep.join(directories[:(directories.index(series.series_path) + 3)])
-    if season_desc not in series.dst.split(os.sep)[-1]:
-        series.dst = directories[:(directories.index(series.series_path) + 2)] + [series.season]
-        series.dst = "%s%s" % (os.sep,os.path.join(*series.dst))
-
+    series.dst = os.path.join(series.series_path, series.name, series.season)
     return series
 
 def naming_episode(args):
-
-    print("  Naming: Started")
 
     ## Get the delimiter of the video filename
     delimiters = [".", "-", "_"]
@@ -72,11 +81,11 @@ def naming_episode(args):
     path = os.path.abspath(os.path.join(args.file, os.pardir))
     series = Namespace(file=args.file, path=path, delim=delimiter, original=args.source_host, series_path=args.root_host)
     series = analyze_series(series)
-    print("  Analysis Namespace: %s" % series)
 
     ## Move the file back to the original path
     file_name = "%s.%s.%s%s" % (series.name, series.episode, series.resolution, series.extension)
     file_dst = os.path.join(series.dst, file_name)
+    debugmsg("The new file path is", (file_dst,))
     return file_dst
 
 if __name__ == "__main__":
