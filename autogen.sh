bfaa397eebf2075908a0daf2dd327972cc806dbd
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2020-11-29 17:48:11 +0100

    Update dependencies for netifaces

--- autogen.sh
+++ autogen.sh
@@ -14,10 +14,12 @@ pip3 install -r requirements.txt
 
 ## Container - Install python3 and pip and all needed modules
 containername=`docker ps | grep -i handbrake | awk '{print $NF}'`
-docker exec -it "$containername" apk add python3 mediainfo
-docker exec -it "$containername" curl https://bootstrap.pypa.io/get-pip.py -o /get-pip.py
-docker exec -it Transmission python3 /get-pip.py &&  rm -rf /get-pip.py
-docker exec -it Transmission pip3 install configparser
+docker exec -it "$containername" apk add --no-cache curl python3 ffmpeg gcc python3-dev musl-dev mediainfo
+docker exec -it "$containername" apk add --no-cache build-base libffi-dev krb5-dev linux-headers zeromq-dev
 
-## Update the hook perfored by handbrake after conversion
+docker exec -it "$containername" curl https://bootstrap.pypa.io/get-pip.py -o /get-pip.py
+docker exec -it "$containername" python3 /get-pip.py &&  rm -rf /get-pip.py
+docker exec -it "$containername" pip3 install configparser netifaces
+
+## Update the hook performed by handbrake after conversion
 cp post_conversion.sh ../config/hooks/post_conversion.sh
